50624e7a33e7b555e8644a5b024729f2b8c4ac83
Ali_moon <161613306+Ali2749193@users.noreply.github.com>
2025-09-15 09:42:18 -0500

    Pantalla de inicio
    Contiene fondo de pantalla con dos botones (Ingrear y Registrar)

---
+++ GA_Control/app/src/main/java/com/example/ga_control/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ga_control
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ga_control.ui.theme.GA_ControlTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GA_ControlTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GA_ControlTheme {
+        Greeting("Android")
+    }
+}
